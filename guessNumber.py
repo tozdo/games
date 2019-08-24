8ac0b17856e5d85935a32f43909ac7f96bde2c7e
Tamara Zdorova <tozdova@gmail.com>
2019-08-25 01:03:12 +0300

    Update guessNumber.py

--- guessNumber.py
+++ guessNumber.py
@@ -6,8 +6,8 @@ got_it = ['верно', 'да', 'правильно']
 def gameGuessing():
     min = 0
     max = 101
-    ready = input('Когда будете готовы, напишите: готов. ')
-    if ready == 'готов':
+    input('Когда будете готовы, нажмите Enter. ')
+    try:
         while True:
             rndGuess = random.randint(min, max)
             matching = input(str(rndGuess) + ' ')
@@ -18,8 +18,8 @@ def gameGuessing():
             elif matching in got_it:
                 print('Ура!')
                 break
-    else:
-        print('что-то пошло не так')
+    except:
+        print('Извините, что-то пошло не так.')
 
 def gameCreating():
     right_number = random.randint(0, 101)
@@ -34,11 +34,12 @@ def gameCreating():
             print('Вы угадали!')
             break
 
-def game():
+def main():
     player1 = input('Кто будет загадывать: я или Вы? ')
     if player1 in list_bot:
         gameCreating()
     else:
         gameGuessing()
 
-game()
+if __name__ == '__main__':
+    main()
